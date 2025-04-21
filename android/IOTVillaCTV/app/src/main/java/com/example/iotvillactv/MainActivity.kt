3220d3adcf1c236a1fb07ccdac1f1f58272dde70
Your Name <eshaanramaul@gmail.com>
2025-04-21 21:24:21 +0300

    Android TV app and debug APK

---
+++ android/IOTVillaCTV/app/src/main/java/com/example/iotvillactv/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.iotvillactv
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+
+        webView.loadUrl("https://iot-villa-c-vu7j.vercel.app")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
