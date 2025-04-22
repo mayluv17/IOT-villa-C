20df948f8db991edca1baccf36db228334f4aadc
ibrahim abdulganiy <mayowaibrahim009@gmail.com>
2025-04-22 13:12:10 +0300

    remove multiple select

--- app/app/(main)/admin/dashboard/access/columns.tsx
+++ app/app/(main)/admin/dashboard/access/columns.tsx
@@ -58,28 +58,6 @@ function DeleteAction({ access }: { access: Access }) {
 
 export const columns: ColumnDef<Access>[] = [
   {
-    id: 'select',
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && 'indeterminate')
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
     accessorKey: 'pinCode',
     header: 'Code',
     cell: ({ row }) => (
